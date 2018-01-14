70800a4a589264de05aea791532345df577d2513
daiki <daiki@DESKTOP-N2CNO33>
2018-01-14 22:33:00 +0900

    Graph

--- src/graph/Graph_view.java
+++ src/graph/Graph_view.java
@@ -4,12 +4,15 @@ import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -26,13 +29,68 @@ public class Graph_view extends Frame implements ActionListener,WindowListener{
 	private Button button1 = new Button("BarChart");
 	private Button button2 = new Button("LineChart");
 	
-	public Graph_view() {
+	public Graph_view(int i) {
 		addWindowListener(this);
 		setTitle("Graph");
+		setLayout(new FlowLayout(FlowLayout.CENTER));
 		add(button1);
 		add(button2);
 	    button1.addActionListener(this);
 		button2.addActionListener(this);
+		
+		DefaultCategoryDataset data = new DefaultCategoryDataset();
+		
+		/*data.addValue(300, "USA", "2005");
+		data.addValue(500, "USA", "2006");
+		data.addValue(120, "USA", "2007");
+		
+		data.addValue(200, "China", "2005");
+		data.addValue(400, "China", "2006");
+		data.addValue(320, "China", "2007");*/
+		
+		int ton;
+		String name, year;
+		ResultSet rs;
+		MySQL mysql = new MySQL();
+		rs = mysql.selectAll();
+		
+		try {
+		while(rs.next()) {
+			name = rs.getString("name");
+			year = rs.getString("year");
+			ton = rs.getInt("ton");
+			data.addValue(ton,name,year);
+			}
+		}catch(SQLException e) {
+		e.printStackTrace();
+		}
+		
+		if(i==0) { 
+			   JFreeChart chart = ChartFactory.createLineChart("Import Volume",
+			                                   "Year",
+			                                   "Ton",
+			                                   data,
+			                                   PlotOrientation.VERTICAL,
+			                                   true,
+			                                   false,
+			                                   false);
+
+			    ChartPanel cpanel = new ChartPanel(chart);
+			    add(cpanel, BorderLayout.CENTER);
+		}else if(i==1){
+			JFreeChart chart = ChartFactory.createBarChart("Import Volume",
+                    "Year",
+                    "Ton",
+                    data,
+                    PlotOrientation.VERTICAL,
+                    true,
+                    false,
+                    false);
+
+			ChartPanel cpanel = new ChartPanel(chart);
+			add(cpanel, BorderLayout.CENTER);
+		}
+		
 	}
 
 	@Override
@@ -78,44 +136,18 @@ public class Graph_view extends Frame implements ActionListener,WindowListener{
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
+	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		DefaultCategoryDataset data = new DefaultCategoryDataset();
-		
-		data.addValue(300, "USA", "2005");
-		data.addValue(500, "USA", "2006");
-		data.addValue(120, "USA", "2007");
-		
-		data.addValue(200, "China", "2005");
-		data.addValue(400, "China", "2006");
-		data.addValue(320, "China", "2007");
-
-		if(arg0.getSource() == button1) {
-			JFreeChart chart = 
-				      ChartFactory.createBarChart("Import Volume",
-				                                   "Year",
-				                                   "Ton",
-				                                   data,
-				                                   PlotOrientation.VERTICAL,
-				                                   true,
-				                                   false,
-				                                   false);
-
-				    ChartPanel cpanel = new ChartPanel(chart);
-				    add(cpanel, BorderLayout.CENTER);
-		}else if(arg0.getSource() == button2) {
-			JFreeChart chart = 
-				      ChartFactory.createLineChart("Import Volume",
-				                                   "Year",
-				                                   "Ton",
-				                                   data,
-				                                   PlotOrientation.VERTICAL,
-				                                   true,
-				                                   false,
-				                                   false);
-
-				    ChartPanel cpanel = new ChartPanel(chart);
-				    add(cpanel, BorderLayout.CENTER);
+		if(e.getSource() == button1) {
+			dispose();
+			Graph_view graph = new Graph_view(0);
+			graph.setBounds(5,5,700,550);
+			graph.setVisible(true);
+		}else if(e.getSource() == button2) {
+			dispose();
+			Graph_view graph = new Graph_view(1);
+			graph.setBounds(5,5,700,550);
+			graph.setVisible(true);
 		}
 	}
 
